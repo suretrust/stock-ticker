07d15d1f000ec2cef7ec6dd01fccb43af3e67d30
suretrust <saholadele@gmail.com>
2023-10-06 17:36:49 +0200

    refactor: move process.env file to a separate file and mock it

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('./api/secret', () => {
+  return {
+    POLYGON_API_KEY: 'test',
+  };
+})
+
 it('renders learn react link', () => {
   render(<App />);
 
---
+++ src/api/secret.tsx
@@ -0,0 +1,1 @@
+export const POLYGON_API_KEY = process.env.REACT_APP_POLYGON_API_KEY;
